8e5ca36e08b4c622877393b4c9b4d6900a7d31df
Giuseppe <figliuologiuseppe@gmail.com>
2020-11-26 09:43:30 +0100

    MiniCssExtractPlugin added to production mode

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,8 +1,18 @@
 const path = require("path");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = merge(common, {
   mode: "production",
   devtool: "source-map",
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
+  },
 });
