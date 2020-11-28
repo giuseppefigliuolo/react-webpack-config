219743a395bbbe0a4c1f08364fa71063b40c4539
Giuseppe <figliuologiuseppe@gmail.com>
2020-11-28 11:45:42 +0100

    cleared it up

--- src/app.js
+++ src/app.js
@@ -1,22 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-//React Redux -> Provider ci permette di fornine i dati dello store a tutti i componenti (nei componenti servirà importare {connect})
-import { Provider } from "react-redux";
-import AppRouter from "./routers/AppRouter";
-import configureStore from "./store/configureStore";
-import { addExpense } from "./actions/expenses";
-import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css";
 import "./styles/styles.scss";
-import "react-dates/lib/css/_datepicker.css";
 
-const store = configureStore();
-console.log("testing");
-
-const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
-);
+const jsx = ``;
 
 ReactDOM.render(jsx, document.getElementById("app"));
